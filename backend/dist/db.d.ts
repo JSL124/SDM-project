a35656c867bfaea33f5411fc1f922f906ac247ec
Jinseo Lee <jason2188@naver.com>
2026-04-12 23:23:04 +0800

    Postgres connect

--- backend/dist/db.d.ts
+++
@@ -1,5 +0,0 @@
-import { Pool, QueryResult } from 'pg';
-declare const pool: Pool;
-export declare function query(sql: string, params?: unknown[]): Promise<QueryResult>;
-export { pool };
-//# sourceMappingURL=db.d.ts.map
